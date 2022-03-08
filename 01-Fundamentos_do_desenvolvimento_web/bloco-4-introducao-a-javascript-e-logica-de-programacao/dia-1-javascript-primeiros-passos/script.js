0eef3a526eab0105288c2228c8b4213782e1f3d5
Marina Andrade Catão <marinavdac@gmail.com>
2022-03-08 19:22:15 -0300

    feat: adiciona primeira parte da resolução de exercícios do bloco 4, dia 1

---
+++ 01-Fundamentos_do_desenvolvimento_web/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -0,0 +1,49 @@
+
+    // Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b , definidas no começo com os valores que serão operados. Faça programas para:
+
+    // Adição (a + b)
+    // Subtração (a - b)
+    // Multiplicação (a * b)
+    // Divisão (a / b)
+    // Módulo (a % b)
+
+const a = 10;
+const b = 15;
+
+let adição = a + b;
+let subtração = a - b;
+let multiplicação = a * b;
+let divisão = a / b;
+let módulo = a % b;
+
+console.log(adição);
+console.log(subtração);
+console.log(multiplicação);
+console.log(divisão);
+console.log(módulo);
+
+// Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados. 
+
+
+const num1 = 120;
+const num2 = 50;
+
+    if (num1 > num2) {
+    
+    let maior = 0;
+
+    maior = num1;
+    console.log(maior);
+} else {
+    maior = num2;
+    console.log(maior);
+};
+
+// Faça um programa que retorne o maior de três números. Defina no começo do programa três constantes com os valores que serão comparados.
+
+const valor1 = 100;
+const valor2 = 30;
+const valor3 = 15;
+
+
+
