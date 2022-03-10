68b68e8d24d475de1c583ead6b7ec7b4ccfbcb3a
Marina Andrade Catão <marinavdac@gmail.com>
2022-03-10 15:09:26 -0300

    feat: adiciona resolução de exercícios do bloco 4, dia 2 - exercício 02

--- 01-Fundamentos_do_desenvolvimento_web/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio-principal/script.js
+++ 01-Fundamentos_do_desenvolvimento_web/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio-principal/script.js
@@ -21,6 +21,31 @@ for (let i = 0; i < numbers.length; i += 1){
     soma += numbers[i]
   
 }
-
 console.log("a soma de todos os elementos desse array é: " + soma)
+
+// 3.Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+
+let media = soma / (numbers.length);
+console.log("a média aritmética dos valores contidos no array é: " + media)
+
+    // 4.Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20"; 
+
+soma >= 20 ? (console.log("valor maior que 20")) : (console.log("valor menor ou igual a 20"));
+
+// 5.Utilizando for , descubra qual o maior valor contido no array e imprima-o;
+
+let maiorValor = numbers[0];
+
+for (let index of numbers) {
+    if (index > maiorValor) {
+        maiorValor = index;
+    } 
+}
+console.log(maiorValor)
+
+// 6. Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado"; 
+
+let isOdd
+
+
 
