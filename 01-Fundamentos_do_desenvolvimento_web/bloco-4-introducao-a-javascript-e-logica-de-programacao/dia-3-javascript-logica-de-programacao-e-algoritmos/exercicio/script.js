c07eb3be46fb50d9f79ee04d1c887f8beab34ffc
Marina Andrade Catão <marinavdac@gmail.com>
2022-03-11 13:36:01 -0300

    feat: adiciona resolução de exercícios do bloco 4, dia 3 - exercício 01

---
+++ 01-Fundamentos_do_desenvolvimento_web/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio/script.js
@@ -0,0 +1,10 @@
+// 1.  Com base nessas informações, crie um algoritmo que retorne o fatorial de 10. 
+
+let numero = 6;
+let soma =1;
+
+for (let i = 1; i <= numero; i++){
+    soma *= i;
+}
+
+console.log(soma)
