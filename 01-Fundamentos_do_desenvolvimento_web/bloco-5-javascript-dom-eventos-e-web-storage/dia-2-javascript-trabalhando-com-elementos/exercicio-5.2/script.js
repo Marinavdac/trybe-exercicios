820bd658a336eae9c9cbcfc5f07aa3a793534777
Marina Andrade Catão <marinavdac@gmail.com>
2022-03-15 21:15:55 -0300

    feat: adiciona exercício bloco 5-dia 05 - questao 05

--- 01-Fundamentos_do_desenvolvimento_web/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/exercicio-5.2/script.js
+++ 01-Fundamentos_do_desenvolvimento_web/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/exercicio-5.2/script.js
@@ -1,8 +1,33 @@
 // 1. Adicione a tag h1 com o texto Exercício 5.2 - JavaScript DOM como filho da tag body ; 
 
-const title = document.querySelector('body');
+const body = document.querySelector('body');
 let jsTitle = document.createElement('h1');
 jsTitle.innerText = 'Exercício 5.2 - JavaScript DOM';
 
-title.appendChild(jsTitle);
+body.appendChild(jsTitle);
+
+// 2. Adicione a tag main com a classe main-content como filho da tag body ;
+
+let sectionMain = document.createElement('main');
+sectionMain.className = "main-content";
+
+body.appendChild(sectionMain);
+
+// 3. Adicione a tag section com a classe center-content como filho da tag main criada no passo 2;
+
+let newSection = document.createElement('section');
+newSection.className = 'center-content';
+sectionMain.appendChild(newSection);
+
+// 4. Adicione a tag p como filho do section criado no passo 3 e coloque algum texto; 
+
+let newParagraph = document.createElement('p');
+newParagraph.innerText = "This is a brand new js paragraph! =D";
+newSection.appendChild(newParagraph);
+
+// 5. Adicione a tag section com a classe left-content como filho da tag main criada no passo 2;
+let main = document.querySelector('main');
+let leftSection = document.createElement('section');
+leftSection.className = 'left-content';
+main.appendChild(leftSection); 
 
