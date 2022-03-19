4041fa17d5cd3e0f0d8c63e64c2ebd96240e3353
Marina Andrade Catão <marinavdac@gmail.com>
2022-03-19 11:16:40 -0300

    feat: adiciona exercicio finalizado dia 5 bloco 1

--- 01-Fundamentos_do_desenvolvimento_web/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
+++ 01-Fundamentos_do_desenvolvimento_web/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -1,7 +1,29 @@
 const container = document.getElementById('container');
+container.style.background = '#f3f3f3';
+
 const header = document.getElementById('header-container');
 header.style.background = '#00b069';
-container.style.background = '#f3f3f3';
+
+const urgentTasks = document.querySelector('.emergency-tasks');
+urgentTasks.style.background = '#ff9f84';
+const h3Titles = urgentTasks.querySelectorAll('h3');
+for (let taskHeaders of h3Titles) {
+    taskHeaders.style.background = '#947cee';
+}
+
+const top3 = document.getElementById('top3');
+
+const newTask = document.createElement('h4');
+top3.appendChild(newTask);
+newTask.innerText = "Finalizar Projeto";
+newTask.style.color = "white";
+
+const NonUrgentTasks = document.querySelector('.no-emergency-tasks');
+NonUrgentTasks.style.background = '#f9db5e';
+const subtitles = NonUrgentTasks.querySelectorAll('h3');
+for (let headers of subtitles) {
+    headers.style.background = 'black';
+}
 
 const footer = document.getElementById('footer-container');
 footer.style.background = '#003533';
