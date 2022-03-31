61b96e6d0d9b99302872d64ba5f5f37d25179181
Marina Andrade Catão <marinavdac@gmail.com>
2022-03-31 09:12:41 -0300

    feat: adiciona 2a parte da resolucao do exercicio bloco 5 dia 4

--- 01-Fundamentos_do_desenvolvimento_web/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/exercicio-dia/script.js
+++ 01-Fundamentos_do_desenvolvimento_web/bloco-5-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/exercicio-dia/script.js
@@ -1,14 +1,25 @@
-
+const text = document.getElementById('text');
 
 window.addEventListener('change', function () {
-    const text = document.getElementById('text');
+    // const text = document.getElementById('text');
     let selector = document.querySelector('select');
     let selected = selector.selectedOptions[0];
-    text.style.backgroundColor = selected.value;  
+    text.style.backgroundColor = selected.value;
+    
+    localStorage.setItem('background-color', selected.value);
 })
 
+const fontBtn = document.getElementById('fontSize');
 
-// const font = document.getElementById('fontSize');
-// font.addEventListener = ('click', function () {
-//     const p
-// })
+fontBtn.addEventListener('click', function () {
+    const input = document.getElementById('input');
+    let fontNumber = input.value + 'px';
+    const textArea = document.getElementById('text');
+    textArea.style.fontSize = fontNumber;
+}
+)
+
+let savedBackground = localStorage.getItem('background-color');
+text.style.backgroundColor = savedBackground;
+
+console.log(savedBackground);
