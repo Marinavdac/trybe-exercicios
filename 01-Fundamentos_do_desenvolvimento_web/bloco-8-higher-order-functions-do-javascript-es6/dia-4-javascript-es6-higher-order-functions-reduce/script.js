9d391602b3f5cb67f2d7c5b1f8eb20570cdf7e6f
Marina Andrade Catão <marinavdac@gmail.com>
2022-04-12 10:46:41 -0300

    feat: adiciona resolucao parcial exercicios course bloco 8 dia 4

--- 01-Fundamentos_do_desenvolvimento_web/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/script.js
+++ 01-Fundamentos_do_desenvolvimento_web/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/script.js
@@ -11,3 +11,24 @@ const getSmaller = (smaller, number) => (
 
 const smaller = numbers.reduce(getSmaller, 0);
 console.log(smaller)
+
+// sums only even numbs:
+
+const numbersArray = [18, 19, 23, 53, 4, 5, 76, 23, 54];
+
+const getEven = (sum, number) => (!(number % 2) ? (sum += number) : sum);
+
+ const evenArray = numbersArray.reduce(getEven, 0)
+console.log(evenArray)
+ 
+const ev = numbersArray.reduce((acc, value) => {
+    if (!(value % 2)) {
+        return acc=  acc+value
+    } else {
+    return acc
+}
+
+}, 0)
+
+console.log(ev)
+ 
