251d6a050e6473b61d51dd8c747e0c34844e948f
Marina Andrade Catão <marinavdac@gmail.com>
2022-04-12 12:12:09 -0300

    feat: adiciona resolucao parcial exercicios course bloco 8 dia 4

--- 01-Fundamentos_do_desenvolvimento_web/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/script.js
+++ 01-Fundamentos_do_desenvolvimento_web/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/script.js
@@ -32,3 +32,112 @@ const ev = numbersArray.reduce((acc, value) => {
 
 console.log(ev)
  
+//gabarito c reduce e filter:
+
+// const numbers = [18, 19, 23, 53, 4, 5, 76, 23, 54];
+
+// const getEven = (number) => number % 2 === 0;
+// const sumPair = (currentValue, number) => currentValue + number;
+
+// const sumNumbers = (array) => array.filter(getEven).reduce(sumPair); // Olhe que código pequeno e conciso!
+
+// console.log(sumNumbers(numbers)); // 152
+
+//gabarito so com reduce:
+
+// const numbers = [18, 19, 23, 53, 4, 5, 76, 23, 54];
+
+// const sumPair = (currentValue, number) => (
+//   (number % 2 === 0) ? currentValue + number : currentValue
+// );
+
+// const sumNumbers = (array) => array.reduce(sumPair, 0);
+
+// console.log(sumNumbers(numbers)); // 152
+
+
+// exercicio alunos:
+
+const estudantes = [
+    {
+      nome: 'Jorge',
+      sobrenome: 'Silva',
+      idade: 14,
+      turno: 'Manhã',
+      materias: [
+        { name: 'Matemática', nota: 67 },
+        { name: 'Português', nota: 79 },
+        { name: 'Química', nota: 70 },
+        { name: 'Biologia', nota: 65 },
+      ],
+    },
+    {
+      nome: 'Mario',
+      sobrenome: 'Ferreira',
+      idade: 15,
+      turno: 'Tarde',
+      materias: [
+        { name: 'Matemática', nota: 59 },
+        { name: 'Português', nota: 80 },
+        { name: 'Química', nota: 78 },
+        { name: 'Biologia', nota: 92 },
+      ],
+    },
+    {
+      nome: 'Jorge',
+      sobrenome: 'Santos',
+      idade: 15,
+      turno: 'Manhã',
+      materias: [
+        { name: 'Matemática', nota: 76 },
+        { name: 'Português', nota: 90 },
+        { name: 'Química', nota: 70 },
+        { name: 'Biologia', nota: 80 },
+      ],
+    },
+    {
+      nome: 'Maria',
+      sobrenome: 'Silveira',
+      idade: 14,
+      turno: 'Manhã',
+      materias: [
+        { name: 'Matemática', nota: 91 },
+        { name: 'Português', nota: 85 },
+        { name: 'Química', nota: 92 },
+        { name: 'Biologia', nota: 90 },
+      ],
+    },
+    {
+      nome: 'Natalia',
+      sobrenome: 'Castro',
+      idade: 14,
+      turno: 'Manhã',
+      materias: [
+        { name: 'Matemática', nota: 70 },
+        { name: 'Português', nota: 70 },
+        { name: 'Química', nota: 60 },
+        { name: 'Biologia', nota: 50 },
+      ],
+    },
+    {
+      nome: 'Wilson',
+      sobrenome: 'Martins',
+      idade: 14,
+      turno: 'Manhã',
+      materias: [
+        { name: 'Matemática', nota: 80 },
+        { name: 'Português', nota: 82 },
+        { name: 'Química', nota: 79 },
+        { name: 'Biologia', nota: 75 },
+      ],
+    },
+];
+  
+  //relatório para dizer em que matéria cada aluno é melhor
+
+//1) pegar todos os alunos
+
+const soAlunos = estudantes.map((estudante) => console.log(estudante.nome)) 
+
+const soMath = estudantes.map((estudante) => console.log(estudante.nome, estudante.materias[0])) 
+
