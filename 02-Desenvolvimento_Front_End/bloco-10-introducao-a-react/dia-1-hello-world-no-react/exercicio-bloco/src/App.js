9cd68770b18e9684f25654b73fbc09b646d2c55d
Marina Andrade Catão <marinavdac@gmail.com>
2022-05-06 19:12:12 -0300

    feat: adiciona primeira parte do exercicio do bloco 10 dia 1

---
+++ 02-Desenvolvimento_Front_End/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicio-bloco/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+const toDo = ['estudar React', 'fazer exercícios do dia', 'refazer projetos passados', 'estudar Typescript']
+
+const Task = (value) => {
+  return (
+    <li>{value}</li>
+  );
+}
+
+function App() {
+ return (
+  <div className="App">
+   <h1>Hello, world</h1>
+     {toDo.map((task) => Task(task))}
+  </div>
+ );
+}
+
+export default App;
