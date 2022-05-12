74ba02cf3bdcebdc5f3e3fe00e1c2325cad0f72a
Marina Andrade Catão <marinavdac@gmail.com>
2022-05-12 12:09:08 -0300

    refactor: adiciona desestruturacao pokemon

--- 02-Desenvolvimento_Front_End/bloco-10-introducao-a-react/dia-2-componentes-react/my-pokedex/src/Pokemon.js
+++ 02-Desenvolvimento_Front_End/bloco-10-introducao-a-react/dia-2-componentes-react/my-pokedex/src/Pokemon.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './App.css';
 class Pokemon extends React.Component {
     render() {
-        const { name, type, averageWeight, image } = this.props;
+        const { name, type, averageWeight: { value, measurementUnit}, image } = this.props;
         return (
             <div className="pokeCard">
             <img src={image} alt={name} />
             <div>Name: {name}</div>
             <div>Tipo: {type}</div>
-            <div>Peso: {averageWeight.value} {averageWeight.measurementUnit}</div>
+            <div>Peso: {value} {measurementUnit}</div>
             </div>
         ) 
     }
